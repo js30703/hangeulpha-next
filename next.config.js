34892d79ae88a45639096818b2f0476dcc60db0c
js30703 <js01053597029@gmail.com>
2022-07-14 19:04:54 +0700

    allowedHost

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    allowedHost: ["127.0.0.1:3000", "localhost:3000"],
+    allowedHost: ["127.0.0.1:3000", "localhost:3000", "hangeulpha.com"],
   },
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
