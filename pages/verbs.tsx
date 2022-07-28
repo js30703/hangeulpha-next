5153eed2548c2fab41dcd4f09604b8871a38c553
js30703 <js01053597029@gmail.com>
2022-07-28 15:25:28 +0700

    wh

--- pages/verbs.tsx
+++ pages/verbs.tsx
@@ -6,17 +6,29 @@ import { auth } from "_firebase/admin";
 //_todo : permission check.
 export default Index;
 
+async function authCheck(req: any, res: any) {
+  try {
+    const idToken = req.cookies?.accessToken;
+    const _ = await auth.verifyIdToken(idToken);
+    if (_?.uid) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
 export async function getServerSideProps(context: any) {
   const { req, res } = context;
-  const idToken = req.cookies?.accessToken;
-  idToken &&
-    auth.verifyIdToken(idToken).then(async (decodedToken) => {
-      //const uid = decodedToken.uid;
-      const result = await prisma.$queryRaw<Verbs[]>`SELECT * FROM "public"."Verbs" where level='1' ORDER BY random() LIMIT 3;`;
-      return {
-        props: { level: "1", verbs: result }, // will be passed to the page component as props
-      };
-    });
+  const isAuthenticated = await authCheck(req, res);
+  if (isAuthenticated) {
+    const result = await prisma.$queryRaw<Verbs[]>`SELECT * FROM "public"."Verbs" where level='1' ORDER BY random() LIMIT 3;`;
+    return {
+      props: { level: "1", verbs: result }, // will be passed to the page component as props
+    };
+  }
   return {
     redirect: {
       permanent: false,
