8870e1647b8b7943580c2547451ebcdc08f342e2
js30703 <js01053597029@gmail.com>
2022-07-29 17:30:05 +0700

    auth isnt

--- components/AuthInitLoader.tsx
+++ components/AuthInitLoader.tsx
@@ -2,44 +2,19 @@ import { useDispatch } from "react-redux";
 import { saveToken } from "store/authSlice";
 import { useEffect } from "react";
 import { getToken } from "_localStorage";
-import { auth as AuthApp, refreshTokenFirebase } from "_firebaseFront";
+import { refreshTokenFirebase } from "_firebaseFront";
 
-// export default function Index() {
-//   const dispatch = useDispatch();
-//   const oneHour = 60 * 60;
-//   useEffect(() => {
-//     const _auth = getToken();
-//     if (_auth.expirationTime < Date.now()) {
-//       AuthApp?.currentUser &&
-//         AuthApp?.currentUser
-//           .getIdToken(/* forceRefresh */ true)
-//           .then((idToken: any) => {
-//             _auth.accessToken = idToken;
-//             _auth.expirationTime = Date.now() + oneHour;
-//             dispatch(saveToken(_auth));
-//           })
-//           .catch(function (error) {
-//             // Handle error
-//           });
-//     } else {
-//       dispatch(saveToken(_auth));
-//     }
-//   });
-
-//   return null;
-// }
-
-export default function Index() {
+export default function AuthInitLoader() {
   const dispatch = useDispatch();
   const oneHour = 60 * 60;
-  const _refresh = async (auth: any) => {
-    if (auth.expirationTime < Date.now()) {
-      const user = await refreshTokenFirebase();
-      auth.accessToken = user.stsTokenManager.accessToken;
-      auth.expirationTime = user.stsTokenManager.expirationTime;
-    }
-  };
   useEffect(() => {
+    const _refresh = async (auth: any) => {
+      if (auth.expirationTime < Date.now()) {
+        const user = await refreshTokenFirebase();
+        auth.accessToken = user.stsTokenManager.accessToken;
+        auth.expirationTime = user.stsTokenManager.expirationTime;
+      }
+    };
     const _auth = getToken();
     _refresh(_auth).catch(console.log);
     dispatch(saveToken(_auth));
